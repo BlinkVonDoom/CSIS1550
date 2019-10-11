d5c946324b37f2926ebe7aa8298ac428dc5054a1
blinkvondom <blynk.rankin@gmail.com>
2019-10-10 18:56:28 -0600

    beginning of assignment5

---
+++ assignment5.sql
@@ -0,0 +1,52 @@
+-- Marina records
+INSERT INTO MARINA VALUES(
+  '1', 'Alexamara East', '108 2nd Ave.', 'Brinman', 'FL', '32273'
+);
+INSERT INTO MARINA VALUES (
+  '2', 'Alexamara Central', '282 Branston', 'W. Brinman', 'FL', '32274'
+);
+
+-- marina_slip records
+INSERT INTO MARINA_SLIP VALUES (
+  '1','1','A1', '40', '3800.00', 'Anderson II', 'Sprite 4000', 'AN75'
+);
+
+INSERT INTO MARINA_SLIP VALUES (
+  '2', '1', 'A2', '40', '3800.00', 'Our Toy', 'Ray 4025', 'EL25'
+);
+
+INSERT INTO MARINA_SLIP VALUES (
+  '3', '1', 'A3', '40', '3600.00', 'Escape', 'Sprite 4000', 'KE22'
+);
+
+INSERT INTO MARINA_SLIP VALUES (
+  '4', '1', 'B1', '30', '2400.00', 'Gypsy', 'Dolphin 28', 'JU92'
+);
+
+INSERT INTO MARINA_SLIP VALUES (
+  '5', '1', 'B2', '30', '2600.00', 'Anderson III', 'Sprite 3000', 'AN75'
+);
+
+INSERT INTO MARINA_SLIP VALUES (
+  '6', '2', '1', '25', '1800.00', 'Bravo', 'Dolphin 25', 'AD57'
+);
+
+INSERT INTO MARINA_SLIP VALUES (
+  '7', '2', '2', '25', '1800.00', 'Chinook', 'Dolphin 22', 'FE82'
+);
+
+INSERT INTO MARINA_SLIP VALUES (
+  '8', '2', '3', '25', '2000.00', 'Listy', 'Dolphin 25', 'SM72'
+);
+
+INSERT INTO MARINA_SLIP VALUES (
+  '9', '2', '4', '30', '2500.00', 'Mermaid', 'Dolphin 28', 'BL72'
+);
+
+INSERT INTO MARINA_SLIP VALUES (
+  '10', '2', '5', '40', '4200.00', 'Axxon II', 'Dolphin 40', 'NO27'
+);
+
+INSERT INTO MARINA_SLIP VALUES (
+  '11', '2', '6', '40', '4200.00', 'Karvel', 'Ray 4025', 'TR72'
+);
